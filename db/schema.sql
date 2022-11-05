8e1f3c0c862daaaee390de95b8173b58cc301d2c
Jennifer Alexander <110498167+jsalexan@users.noreply.github.com>
2022-11-05 16:59:15 -0700

    Ugh, I'm in naming hell. The schema and seeds aren't loading correctly. Lots of error messages related to the keys. I'm in the middle of renaming them and I have to stop (not ideal), so pick back up here next time

--- db/schema.sql
+++ db/schema.sql
@@ -4,21 +4,24 @@ CREATE DATABASE employee_db;
 USE employee_db;
 
 CREATE TABLE department (
-    id INT PRIMARY KEY NOT NULL,
+    dept_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     department_name VARCHAR(30) NOT NULL,
+    
 );
 
 CREATE TABLE employee_role (
-    id INT PRIMARY KEY NOT NULL,
+    em_role_id PRIMARY KEY INT NOT NULL AUTO_INCREMENT,
     title VARCHAR(30) NOT NULL,
-    salary DECIMAL NOT NULL,
+    salary DECIMAL(19,4) NOT NULL,
     department_id INT NOT NULL,
-):
+    FOREIGN KEY(department_id) REFERENCES department(dept_id) ON DELETE CASCADE
+);
 
 CREATE TABLE employee (
-    id INT PRIMARY KEY NOT NULL,
+    em_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT NOT NULL,
     manager_id INT NOT NULL,
-):
+    FOREIGN KEY(role_id) REFERENCES employee_role(em_role_id) ON DELETE CASCADE
+);
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,35 +1,35 @@
-INSERT INTO department (id, department_name)
-VALUES (1, "Marketing"),
-(2, "Development"),
-(3, "Production"),
-(4, "Artistic"),
-(5, "Education"),
-(6, "Finance");
+INSERT INTO department (department_name)
+VALUES ("Marketing"),
+("Development"),
+("Production"),
+("Artistic"),
+("Education"),
+("Finance");
 
-INSERT INTO employee_role (id, title, salary, department_id)
-VALUES (1000, "Artistic Director", 250,000.00, 4),
-(1001, "Marketing Director", 80,000.00, 1),
-(1002, "Development Director", 80,000.00, 2),
-(1003, "Production Manager", 100,000.00, 3),
-(1004, "Education Director", 70,000.00, 5),
-(1005, "Finance Director", 90,000.00, 6),
-(1006, "Controller", 70,000.00, 6),
-(1007, "Grants Writer", 70,000.00, 2),
-(1008, "Producer", 150,000.00, 4),
-(1009, "Teaching Assistant", 40,000.00, 5),
-(1010, "Stage Supervisor", 70,000.00, 3),
-(1011, "Events Manager", 50,000.00, 1);
+INSERT INTO employee_role (title, salary, department_id)
+VALUES ("Artistic Director", 250000.00, 4),
+("Marketing Director", 80000.00, 1),
+("Development Director", 80000.00, 2),
+("Production Manager", 100000.00, 3),
+("Education Director", 70000.00, 5),
+("Finance Director", 90000.00, 6),
+("Controller", 70000.00, 6),
+("Grants Writer", 70000.00, 2),
+("Producer", 150000.00, 4),
+("Teaching Assistant", 40000.00, 5),
+("Stage Supervisor", 70000.00, 3),
+("Events Manager", 50000.00, 1);
 
-INSERT INTO employee(id, first_name, last_name, role_id, manager_id)
-VALUES ("Barbara", "Cullins", 1001, 901),
-(300, "Emily", "Munn", 1000, 900),
-(301, "Roz", "Bluth", 1002, 902),
-(302, "David", "Roth", 1003, 903),
-(303, "Chris", "Hay", 1004, 904),
-(304, "Tom", "Moses", 1005, 905),
-(305, "Brenda", "Mike", 1006, 905),
-(306, "Michael", "Marian", 1007, 902),
-(307, "Mara", "Issly", 1008, 900),
-(308, "Paula", "Stevens", 1009, 904), 
-(309, "Steve", "Smith", 1010, 903),
-(310, "Jennifer", "Spring", 1011, 901);
+INSERT INTO employee(first_name, last_name, role_id, manager_id)
+VALUES ("Barbara", "Cullins", 1001, 1),
+("Emily", "Munn", 1000, 1),
+("Roz", "Bluth", 1002, 1),
+("David", "Roth", 1003, 1),
+("Chris", "Hay", 1004, 1),
+("Tom", "Moses", 1005, 1),
+("Brenda", "Mike", 1006, 905),
+("Michael", "Marian", 1007, 902),
+("Mara", "Issly", 1008, 900),
+("Paula", "Stevens", 1009, 904), 
+("Steve", "Smith", 1010, 903),
+("Jennifer", "Spring", 1011, 901);
