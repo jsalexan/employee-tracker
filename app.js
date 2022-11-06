04533e5d37a7a0546a75f36659adc3ec99e6a6ce
Jennifer Alexander <110498167+jsalexan@users.noreply.github.com>
2022-11-06 13:26:00 -0800

    Added init function with main menu inquirer choices

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const inquirer = require("inquirer");
 const mysql = require('mysql2');
+const { rawListeners, allowedNodeEnvironmentFlags } = require("process");
 
 const db = mysql.createConnection(
     {
@@ -11,4 +12,42 @@ const db = mysql.createConnection(
     console.log(`Connected to the employee_db database.`)
   );
 
-  
+  function init() {
+    inquirer.prompt ([ 
+        {
+            type: "rawlist",
+            name: "menu",
+            message: "What would you like to do?",
+            choices: ["View all departments", "View all roles", "View all employees", "Add a department", "Add a role", "Add an employee", "Update an employee role"]
+        }
+    ])
+
+    .then(function(answers){
+        switch (answers.menu) {
+            case "View all departments":
+                viewDepartments();
+                break;
+            case "View all roles":
+                viewRoles();
+                break;
+            case "View all employees":
+                viewEmployees();
+                break;
+            case "Add a department":
+                addDepartment();
+                break;
+            case "Add a role":
+                addRole();
+                break;
+            case "Add an employee":
+                addEmployee();
+                break;
+            case "Update an employee role":
+                updateRole();
+                break;
+            // default:
+                // xxxxxxx(); ADD DEFAULT?
+    }
+})
+}; 
+
