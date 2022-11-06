51ae0687722e21e1a78352c7837bd0a8eb8ccfb7
Jennifer Alexander <110498167+jsalexan@users.noreply.github.com>
2022-11-06 13:04:31 -0800

    Got the seeds and schema corrected and running! Starting on the main code, but committing first.

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 const inquirer = require("inquirer");
+const mysql = require('mysql2');
+
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      user: 'root',
+      password: '',
+      database: 'employee_db'
+    },
+    console.log(`Connected to the employee_db database.`)
+  );
+
+  
